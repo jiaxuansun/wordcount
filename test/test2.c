6d67f35b71e375e1eac19649263af0d3ee36f8e1
minghuatang <871228261@qq.com>
2018-03-20 22:06:55 +0800

    feat: upload test

---
+++ test/test2.c
@@ -0,0 +1,20 @@
+test()
+{
+File writename = new File(outputPath);
+            writename.createNewFile();
+codeLine */
+            BufferedWriter out = new BufferedWriter(new FileWriter(writename));
+    // note line
+        out write(outputBuffer);
+/* noteLine
+/* noteLine
+*/
+/* noteLine */
+/* noteLine
+// noteLine
+*/
+        out.flush();
+        out.close();
+}
+// noteLine
+for(){}/* noteLine */
